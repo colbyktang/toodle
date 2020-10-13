7ca7590d700543105596b25b801f549010af3c57
Will Luong <tluong@stedwards.edu>
2020-10-13 11:45:22 -0500

    React-homepage component
    This package contains the react build of the Toodle homepage

---
+++ precache-manifest.42fa8b97a01a58f1cd3b338fddcf0d35.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194fb528d52dbe6d7c4a7954bb9945a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1a524488dffd5570e7f",
+    "url": "/static/css/main.30e67efd.chunk.css"
+  },
+  {
+    "revision": "99ceb1d3b369199cd83c",
+    "url": "/static/js/2.2b6d19be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b6d19be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1a524488dffd5570e7f",
+    "url": "/static/js/main.ed36aea0.chunk.js"
+  },
+  {
+    "revision": "409401602ba9c08d1d35",
+    "url": "/static/js/runtime-main.d4dc386d.js"
+  },
+  {
+    "revision": "7b8d9a653c51e590c16ebbfe8a820a14",
+    "url": "/static/media/Best-Programming-Languages-to-Start-Learning-Today.7b8d9a65.jpg"
+  },
+  {
+    "revision": "096339a264ac415529311109c07414cd",
+    "url": "/static/media/Competitive-Programming-1.096339a2.jpg"
+  },
+  {
+    "revision": "49b3606846bcd40e53fcf70cb579379f",
+    "url": "/static/media/download.49b36068.jpeg"
+  },
+  {
+    "revision": "9c77ce0370ad8d029f407e772583a296",
+    "url": "/static/media/favicon.9c77ce03.ico"
+  },
+  {
+    "revision": "d9254df4c8ad149de9911500594c9637",
+    "url": "/static/media/img02.d9254df4.jpg"
+  },
+  {
+    "revision": "76b2bfd3e421399a71e414ae954c11fe",
+    "url": "/static/media/placeholder.76b2bfd3.png"
+  }
+]);
