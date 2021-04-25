5105f28bb16ca53d7601021648a87d5e2b5d7843
Will Luong <tluong@stedwards.edu>
2021-04-25 18:49:53 -0500

    Updated the file with Applicant data model and Course

--- structs/structs.go
+++ structs/structs.go
@@ -36,3 +36,29 @@ type Question struct {
 	StudentName string             `json:"studentName,omitempty" bson:"studentName,omitempty"`
 	StudentID   string             `json:"student_ID,omitempty" bson:"student_ID,omitempty"`
 }
+
+//Applicant data model:
+type Applicant struct {
+	ID                 primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	ApplicantName      string             `json:"applicantName,omitempty" bson:"applicantName,omitempty"`
+	ApplicantEmail     string             `json:"applicantEmail,omitempty" bson:"applicantEmail,omitempty"`
+	ApplicantPhone     string             `json:"applicantPhone,omitempty" bson:"applicantPhone,omitempty"`
+	Position           string             `json:"position,omitempty" bson:"position,omitempty"`
+	ApplicantLinkedin  string             `json:"applicantLinkedin,omitempty" bson:"applicantLinkedin,omitempty"`
+	ApplicantTwitter   string             `json:"applicantTwitter,omitempty" bson:"applicantTwitter,omitempty"`
+	ApplicantGitHub    string             `json:"applicantGitHub,omitempty" bson:"applicantGitHub,omitempty"`
+	ApplicantPortfolio string             `json:"applicantPortfolio,omitempty" bson:"applicantPortfolio,omitempty"`
+	OtherInformation   string             `json:"applicantOther,omitempty" bson:"applicantOther,omitempty"`
+	ExtraComment       string             `json:"extraComment,omitempty" bson:"extraComment,omitempty"`
+	ApplicantStatus    string             `json:"status,omitempty" bson:"status,omitempty"`
+}
+
+//Course data model:
+type Course struct {
+	ID                primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	CourseName        string             `json:"courseName,omitempty" bson:"courseName,omitempty"`
+	CourseDescription string             `json:"courseDescription,omitempty" bson:"courseDescription,omitempty"`
+	Professors        []string           `json:"professors,omitempty" bson:"professors,omitempty"` //who is current teaching this course
+	Schools           []string           `json:"schools,omitempty" bson:"schools,omitempty"`       //Which schools is offering this course
+	Semesters         []string           `json:"semesters,omitempty" bson:"semesters,omitempty"`   //Semester offered
+}
